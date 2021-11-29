370045d65b0b4f10d8c24686487f3cb3a8f4e566
Hoang Nguyen <hoang24@pdx.edu>
2021-11-28 21:59:36 -0800

    only save the first 8 seconds of all the recordings instead of split them into chunks

--- generate_chunked_mri.py
+++ generate_chunked_mri.py
@@ -1,11 +1,14 @@
 import soundfile as sf
 import numpy as np
+from scipy.io import wavfile
 import librosa
 import os
 import subprocess
 import noisereduce
 
-cosep_root = 'mri'
+audLen = 8 # length of trimmed recording (seconds)
+
+cosep_root = '/content/drive/MyDrive/EE/EE225D_Project/mri'
 
 input_dir = cosep_root
 output_dir = cosep_root + '_preprocessed'
@@ -13,25 +16,22 @@ output_dir = cosep_root + '_preprocessed'
 sub_list = os.listdir(input_dir)
 
 for folder in sub_list:
-    if not 'sub' in folder:
-        break
-    os.makedirs(output_dir + "/" + folder)
+    if 'sub' not in folder:
+        continue
+
+    try:
+        os.makedirs(output_dir + "/" + folder)
+    except FileExistsError:
+        continue
+
     video_input_dir = input_dir + "/" + folder + "/"
     dir_list = os.listdir(video_input_dir)
 
     for video_file in dir_list:
         video_title = video_file.split('.')[0]
-        os.makedirs(output_dir + "/" + folder + "/" + video_title)
         y, sr = librosa.load(input_dir + "/" + folder + "/" + video_file, mono=False)
-        y_reduced = noisereduce.reduce_noise(y=y, sr=sr)
-        y_extended = np.pad(y_reduced, (0, sr*5 - len(y_reduced)%(sr*5)), 'constant')
-        y_split = np.reshape(y_extended, (-1, 5*sr))
-        i = 0
-        for chunk in y_split:
-            i += 1
-            print(folder + video_title + i)
-            sf.write(output_dir + "/" + folder + "/" + video_title + '_chunk' + i + '.wav', chunk, sr)
-        #command = "ffmpeg -i " + video_input_dir + video_title + ".mp4 -ab 160k -ac 2 -ar 44100 -vn " + output_dir + "/" + folder + "/" + video_title + ".wav"
-        
-        # subprocess.call(command, shell=True)
+        N_trim = audLen * sr;
+        audio = y[:, 0:N_trim]
+        sf.write(output_dir + "/" + folder + "/" + video_title + f'_first{audLen}sec.wav', np.transpose(audio), sr)
+
     print("Completed " + folder)
